4fa41f793ee0bb6d11b0fdda04647fce9e87f129
magatama8810 <haya1206s@icloud.com>
2024-08-21 15:58:35 +0900

    掲示板

--- src/table.php
+++ src/table.php
@@ -1,50 +1,45 @@
 <?php
+session_start();
 
 $mysqli = new mysqli('localhost', 'magatama', '8810', 'test');
 
 if ($mysqli->connect_error) {
-    echo $mysqli->connect_error;
-    exit();
+    die("接続失敗: " . $mysqli->connect_error);
 }
 
-// 課題：結合したテーブルをSELECTして$result変数に格納する処理を書く
-$sql = "
-    SELECT 
-        trx_comments.id,
-        trx_users.user_name,
-        trx_comments.text
-    FROM 
-        trx_comments
-    INNER JOIN 
-        trx_users
-    ON 
-        trx_comments.user_id = trx_users.id
-";
 
+$sql = "SELECT trx_comments.id, trx_users.user_name, trx_comments.text 
+        FROM trx_comments 
+        INNER JOIN trx_users ON trx_comments.user_id = trx_users.id";
 $result = $mysqli->query($sql);
-
-if (!$result) {
-    echo "クエリの実行に失敗しました: " . $mysqli->error;
-    exit();
-}
 
 echo "<table>\n";
 echo "<tr><th>ID</th><th>ユーザ名</th><th>コメント</th></tr>\n";
 while ($row = $result->fetch_assoc()) {
-    $html = <<<TEXT
-<tr>
-  <td>{$row['id']}</td>
-  <td>{$row['user_name']}</td>
-  <td>{$row['text']}</td>
-</tr>
-TEXT;
-    echo $html;
+    echo "<tr>\n";
+    echo "<td>{$row['id']}</td>\n";
+    echo "<td>{$row['user_name']}</td>\n";
+    echo "<td>{$row['text']}</td>\n";
+    echo "</tr>\n";
 }
-echo "</table>";
+echo "</table>\n";
 
-$result->free();
+if (isset($_SESSION['user_id'])) {
+
+    if (empty($_SESSION['csrf_token'])) {
+        $_SESSION['csrf_token'] = bin2hex(random_bytes(32));
+    }
+    $csrf_token = $_SESSION['csrf_token'];
+
+    echo <<<FORM
+    <h2>コメント投稿</h2>
+    <form action="comments.php" method="post">
+        <textarea name="comment_text" required></textarea><br>
+        <input type="hidden" name="csrf_token" value="$csrf_token">
+        <input type="submit" value="投稿">
+    </form>
+FORM;
+}
 
 $mysqli->close();
-
 ?>
-
